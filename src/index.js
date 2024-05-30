05a0acadeabf85e70126508fc688349770077c92
Yurii Vychivskii <yuravychivskii@gmail.com>
2024-05-30 13:22:41 +0300

    changed syntax for new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
 import App from './components/app/App';
+
 import './style/style.scss';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
