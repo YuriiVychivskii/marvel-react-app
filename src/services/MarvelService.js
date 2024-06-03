69df92c0e4edf1bc57c9261e86431039b8fe6cd4
Yurii Vychivskii <yuravychivskii@gmail.com>
2024-06-03 14:25:54 +0300

    added transformation for characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,23 +4,38 @@ class MarvelService {
 		'apikey=b385361f7e9b608aa0a767131dfa968e&hash=9c035c0ddd58115d76cf41112b2af2f8';
 
 	getResource = async url => {
-		const data = await fetch(url);
-		if (!data.ok) {
-			throw Error(`Cloud not fetch: ${url}; status: ${data.status}`);
+		const res = await fetch(url);
+		if (!res.ok) {
+			throw Error(`Cloud not fetch url: ${url} staus: ${res.status}`);
 		}
-		return data.json();
+		return res.json();
 	};
 
-	getAllCharacters = () => {
-		return this.getResource(
-			`${this._apiBase}characters?limit=9&offset=210&ts=1&${this._apiKey}`
+	getAllCharacters = async () => {
+		return await this.getResource(
+			`${this._apiBase}characters?ts=1&limit=9&offset=210&${this._apiKey}`
+		).then(data =>
+			data.data.results.map(item => this._TransformedCharacter(item))
 		);
 	};
 
-	getCharacterById = id => {
-		return this.getResource(
-			`${this._apiBase}characters/${id}?ts=1&apikey=${this._apiKey}`
-		);
+	getCharacterByID = async id => {
+		return await this.getResource(
+			`${this._apiBase}characters/${id}?ts=1&${this._apiKey}`
+		).then(data => this._TransformedCharacter(data.data.results[0]));
+	};
+
+	_TransformedCharacter = data => {
+		return {
+			name: data.name,
+			description:
+				data.description.length > 210
+					? `${data.description.slice(0, 210)}...`
+					: data.description,
+			thumbnail: `${data.thumbnail.path}.${data.thumbnail.extension}`,
+			homepage: data.urls[0].url,
+			wiki: data.urls[1].url,
+		};
 	};
 }
 
