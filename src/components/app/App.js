8417381e229a1a28d3329c636451759d1351ad83
Yurii Vychivskii <yuravychivskii@gmail.com>
2024-06-13 21:01:11 +0300

    added errorBoundary, catching error

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,10 @@
 import { Component } from 'react';
+import decoration from '../../resources/img/vision.png';
 import AppHeader from '../appHeader/AppHeader';
 import CharInfo from '../charInfo/CharInfo';
 import CharList from '../charList/CharList';
+import ErrorBoundary from '../errorBoundary/ErorrBoundary';
 import RandomChar from '../randomChar/RandomChar';
-
-import decoration from '../../resources/img/vision.png';
 
 class App extends Component {
 	state = {
@@ -22,8 +22,12 @@ class App extends Component {
 				<main>
 					<RandomChar />
 					<div className="char__content">
-						<CharList onSelectChar={this.onSelectChar} />
-						<CharInfo selectedChar={this.state.selectedChar} />
+						<ErrorBoundary>
+							<CharList onSelectChar={this.onSelectChar} />
+						</ErrorBoundary>
+						<ErrorBoundary>
+							<CharInfo selectedChar={this.state.selectedChar} />
+						</ErrorBoundary>
 					</div>
 					<img className="bg-decoration" src={decoration} alt="vision" />
 				</main>
