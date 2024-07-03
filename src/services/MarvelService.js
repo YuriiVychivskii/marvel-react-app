5268de1d4a0a4388b39b88bf610ebf91ea2e336c
Yurii Vychivskii <yuravychivskii@gmail.com>
2024-07-03 18:08:25 +0300

    Added services for Comics

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -7,6 +7,7 @@ export const useMarvelService = () => {
 	const _apiKey =
 		'apikey=b385361f7e9b608aa0a767131dfa968e&hash=9c035c0ddd58115d76cf41112b2af2f8';
 	const _offsetChar = 210;
+	const _offsetComic = 100;
 
 	const getAllCharacters = async (offset = _offsetChar) => {
 		const res = await request(
@@ -17,6 +18,18 @@ export const useMarvelService = () => {
 
 	const getCharacterByID = async id => {
 		const res = await request(`${_apiBase}characters/${id}?ts=1&${_apiKey}`);
+		return _transformCharacter(res.data.results[0]);
+	};
+
+	const getAllComics = async (offset = _offsetComic) => {
+		const res = await request(
+			`${_apiBase}comics?ts=1&limit=8&offset=${offset}&${_apiKey}`
+		);
+		return res.data.results.map(item => _transformComic(item));
+	};
+
+	const getComicByID = async id => {
+		const res = await request(`${_apiBase}comics/${id}?ts=1&${_apiKey}`);
 		return _transformCharacter(res.data.results[0]);
 	};
 
@@ -41,5 +54,35 @@ export const useMarvelService = () => {
 		};
 	};
 
-	return { loading, error, clearError, getAllCharacters, getCharacterByID };
+	const _transformComic = ({
+		id,
+		title,
+		thumbnail,
+		prices,
+		description,
+		pageCount,
+		textObjects,
+	}) => {
+		return {
+			id: id,
+			title: title,
+			description: description || 'There is no description for this comics',
+			thumbnail: thumbnail.path + '.' + thumbnail.extension,
+			prices: prices[0].price ? `${prices[0].price}$` : 'not available',
+			pageCount: pageCount
+				? `${pageCount} p.`
+				: 'No information about the number of pages',
+			language: textObjects[0]?.language || 'en-us',
+		};
+	};
+
+	return {
+		loading,
+		error,
+		clearError,
+		getAllCharacters,
+		getCharacterByID,
+		getAllComics,
+		getComicByID,
+	};
 };
